45f2c6960f66f220268cd21ab5897f3a8039a6fb
Yash Vasani <yashvasani1@gmail.com>
2021-06-20 23:31:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// SOCIAL PANEL CONTAINER
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
+
+// COUNT API
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch('https://api.countapi.xyz/update/iyashvasani.info/2001/?amount=1')
+        .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value;
+        })
+}
